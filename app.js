813ca794b1a401ca51aef9ee43cced98fceebf4b
Arthur Sharipov <sharipov@sheridancollege.ca>
2024-09-24 03:27:55 -0400

    Added schema and model using mongoose. Added config for global env

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-app.use(morgan('tiny'));
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('tiny'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
+
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
-})
+});
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
